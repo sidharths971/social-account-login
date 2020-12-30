171802de7d4a0cde69de6b955860224cfffdbbd6
sidharth <sidharths971@gmail.com>
2020-12-30 10:51:06 +0530

    second commit

--- social_login/settings.py
+++ social_login/settings.py
@@ -174,6 +174,15 @@ SOCIALACCOUNT_PROVIDERS = {
         'LOCALE_FUNC': 'path.to.callable',
         'VERIFIED_EMAIL': False,
         'VERSION': 'v7.0',
-    }
+    },
+
+    'facebook':
+        {
+            'SCOPE': ['email'],
+            'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+            'METHOD': 'oauth2',
+            'LOCALE_FUNC': lambda request: 'en_US'
+        }
+
 
 }
